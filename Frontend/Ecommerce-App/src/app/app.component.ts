1061937af1e27da7a800e33b58810a54318558a6
rishcodelib <ri.sbh23@gmail.com>
2021-04-14 00:24:29 +0530

    updated

--- Frontend/Ecommerce-App/src/app/app.component.ts
+++ Frontend/Ecommerce-App/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {NavbarComponent} from  './component/navbar/navbar.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
