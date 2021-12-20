f70bf3dceb67fbe58bd35801b55d53452cd98c48
rishcodelib <ri.sbh23@gmail.com>
2021-12-20 19:55:00 +0530

    updating Project

--- Frontend/Ecommerce-App/src/app/app.module.ts
+++ Frontend/Ecommerce-App/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { HomeComponent } from './component/home/home.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HomeComponent
   ],
   imports: [
